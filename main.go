bdfcda583e51a7f4de35955987eeb9045ce9d47d
Evilcmd <tejonweb28@gmail.com>
2024-08-09 13:33:47 +0530

    gemini is working

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/google/generative-ai-go/genai"
 	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
 )
 
 func main() {
+	ctx := context.Background()
+
 	godotenv.Load()
 
 	GeminiApiKey := os.Getenv("GEMINI_API_KEY")
-	fmt.Println(GeminiApiKey)
+
+	client, err := genai.NewClient(ctx, option.WithAPIKey(GeminiApiKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel("gemini-1.5-flash")
+
+	resp, err := model.GenerateContent(ctx, genai.Text("Write a story about a AI and magic"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for res := range resp.Candidates {
+		fmt.Println(res)
+	}
+
+	fmt.Println(resp.Candidates[0].Content.Parts[0])
+
 }
